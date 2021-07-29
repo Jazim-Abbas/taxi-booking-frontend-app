e9436a3684f1ee6e4731b5a31d52ad652e58c235
Jazim-Abbas <jazimabbas87@gmail.com>
2021-07-29 21:40:42 +0500

    code refactor: move login form to its own component ..

---
+++ src/components/login/login-form.jsx
@@ -0,0 +1,35 @@
+import { Field } from "formik";
+
+import { AppForm, FieldError } from "../app-form";
+import { loginSchema } from "../../utils/validations";
+
+export default function LoginForm({ onSubmit }) {
+  return (
+    <AppForm
+      initialValues={initialValues}
+      validationSchema={loginSchema}
+      handleSubmit={onSubmit}
+    >
+      <div className="login_fields">
+        <div className="input_field">
+          <label>Email address</label>
+          <Field type="email" name="email" />
+          <FieldError field="email" />
+        </div>
+        <div className="input_field">
+          <label>Password</label>
+          <Field type="password" name="password" />
+          <FieldError field="password" />
+        </div>
+        <div className="submit_btn">
+          <button type="submit">Login</button>
+        </div>
+      </div>
+    </AppForm>
+  );
+}
+
+const initialValues = {
+  email: "",
+  password: "",
+};
--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,13 +1,11 @@
-import { Field, ErrorMessage } from "formik";
 import { Link } from "react-router-dom";
 
 import "./login.css";
-import { loginSchema } from "../../utils/validations";
-import { AppForm, FieldError } from "../app-form";
+import LoginForm from "./login-form";
 
 function LoginScreen() {
-  const handleLogin = () => {
-    console.log("handle login");
+  const handleLogin = ({ formValues }) => {
+    console.log("handle login", formValues);
   };
 
   return (
@@ -17,27 +15,7 @@ function LoginScreen() {
           <Link to="/Home">PICK UP</Link>
           <div className="login_form">
             <h3>Log In</h3>
-            <AppForm
-              initialValues={initialValues}
-              validationSchema={loginSchema}
-              handleSubmit={handleLogin}
-            >
-              <div className="login_fields">
-                <div className="input_field">
-                  <label>Email address</label>
-                  <Field type="email" name="email" />
-                  <FieldError field="email" />
-                </div>
-                <div className="input_field">
-                  <label>Password</label>
-                  <Field type="password" name="password" />
-                  <FieldError field="password" />
-                </div>
-                <div className="submit_btn">
-                  <button type="submit">Login</button>
-                </div>
-              </div>
-            </AppForm>
+            <LoginForm onSubmit={handleLogin} />
           </div>
         </div>
       </section>
@@ -46,8 +24,3 @@ function LoginScreen() {
 }
 
 export default LoginScreen;
-
-const initialValues = {
-  email: "",
-  password: "",
-};
