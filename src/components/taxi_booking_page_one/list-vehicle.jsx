21091ccfecb397228239b9d2e09b41f64586ad8b
Jazim-Abbas <jazimabbas87@gmail.com>
2021-07-31 11:24:25 +0500

    select-vehicle: added  dynamic vehicles but not from server ..

--- src/components/taxi_booking_page_one/list-vehicle.jsx
+++ src/components/taxi_booking_page_one/list-vehicle.jsx
@@ -1,13 +1,17 @@
+import { useState } from "react";
+
 import SingleVehicle from "./single-vehicle";
+import { vehicleList } from "./vehicles.data";
 
 export default function ListVehicle() {
+  const [vehicles, setVehicles] = useState(vehicleList);
+
   return (
     <>
       <form>
-        <SingleVehicle />
-        <SingleVehicle />
-        <SingleVehicle />
-        <SingleVehicle />
+        {vehicles.map((vehicle) => (
+          <SingleVehicle vehicle={vehicle} key={vehicle.id} />
+        ))}
       </form>
     </>
   );
--- src/components/taxi_booking_page_one/single-vehicle.jsx
+++ src/components/taxi_booking_page_one/single-vehicle.jsx
@@ -1,21 +1,22 @@
 import { Link } from "react-router-dom";
 
-export default function SingleVehicle() {
+export default function SingleVehicle({ vehicle }) {
   return (
     <div class="booking_page_one_car_selection">
       <div class="booking_page_one_car_detail">
         <div class="booking_page_one_single_car_detail">
           <figure>
-            <img src="assets/image/standard.png" />
+            <img src={vehicle.image} />
           </figure>
           <div class="page_one_single_car_detail">
-            <h1>Standard</h1>
+            <h1>{vehicle.name}</h1>
             <span>
               <i class="fas fa-user"></i>
-              <small>4 Paks</small>
+              <small>{vehicle.passengerCapacity} Paks</small>
             </span>
             <span>
-              <i class="fas fa-suitcase-rolling"></i> <small>4 Bags</small>
+              <i class="fas fa-suitcase-rolling"></i>{" "}
+              <small>{vehicle.luggageCapacity} Bags</small>
             </span>
             <span>
               <i id="single_car_more" class="fas fa-chevron-down"></i>
@@ -27,7 +28,7 @@ export default function SingleVehicle() {
       <div class="booking_page_one_car_detail">
         <div class="booking_page_one_car_price_detail">
           <strong>Total One-Way Price</strong>
-          <strong>$100</strong>
+          <strong>${vehicle.pricePerKM}</strong>
           <small>Free Cancelation</small>
           <small>No Hidden Charges</small>
           <Link to="/taxi_booking_page_two">Select This Vehicle</Link>
