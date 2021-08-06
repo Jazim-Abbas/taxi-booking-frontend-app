e1ac02d3d5262d23d34bacde3248829373fc3d19
Jazim-Abbas <jazimabbas87@gmail.com>
2021-08-06 18:55:09 +0500

    protect route and isauth and isadmin status before navigate to destination path ..

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 import { Switch, Route } from "react-router-dom";
-import AccountRoutes from "./account-routes";
 
 import ProtectedRoute from "./protected-route";
 import { BASE_ROUTE, PUBLIC_ROUTES, PROTECTED_ROUTES } from "./routes-list";
 
 export default function AppRoutes() {
+  const user = JSON.parse(localStorage.getItem("user"));
+
   return (
     <Switch>
       <Route exact path={BASE_ROUTE.path}>
@@ -30,6 +31,8 @@ export default function AppRoutes() {
           path={route.path}
           reduxKeyNames={route.reduxKeyNames}
           component={route.Component}
+          user={user}
+          isAdmin
         />
       ))}
     </Switch>
--- src/routes/protected-route.jsx
+++ src/routes/protected-route.jsx
@@ -1,21 +1,41 @@
-import { Route } from "react-router-dom";
+import { Redirect, Route } from "react-router-dom";
 
 import useReduxDataCheck from "../hooks/useReduxDataCheck";
 
 export default function ProtectedRoute({
-  hasCheckAuth = false,
+  isAuth = false,
+  isAdmin = false,
   reduxKeyNames = [],
   component: Component,
   ...rest
 } = {}) {
   useReduxDataCheck([...reduxKeyNames]);
+  const { user } = rest;
 
   return (
     <Route
       {...rest}
       render={(props) => {
+        if (!isAuth && !isAdmin) {
+          return <Component {...props} />;
+        }
+
+        if (!user) return <_RedirectToLogin {...props} />;
+
+        if (isAdmin) {
+          if (!user.isAdmin) {
+            return <_RedirectToLogin {...props} />;
+          }
+        }
+
         return <Component {...props} />;
       }}
     />
+  );
+}
+
+function _RedirectToLogin({ ...props }) {
+  return (
+    <Redirect to={{ pathname: "/login", state: { from: props.location } }} />
   );
 }
