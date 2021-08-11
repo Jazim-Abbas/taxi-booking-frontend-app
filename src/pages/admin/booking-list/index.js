3f8ac250333abd0df42b5c1bd3bfd9f3cc21a435
Jazim-Abbas <jazimabbas87@gmail.com>
2021-08-11 14:46:56 +0500

    admin-panel: added admin layout ..

--- src/pages/admin/booking-list/index.js
+++ src/pages/admin/booking-list/index.js
@@ -1,5 +1,94 @@
 export default function AdminBookingList() {
   return (
+    <AdminLayout>
+      {/* body */}
+      <div class="body">
+        <div class="admin_container">
+          {/* <!-- dashboard window start--> */}
+          <div class="dashboard_window" id="dashboard_window">
+            <div class="dashboard_form">
+              <strong>Booking History:</strong>
+              <div class="list_selecter">
+                <select class="form-control" name="state" id="maxRows">
+                  <option value="5000">Show ALL Rows</option>
+                  <option value="5">5</option>
+                  <option value="10">10</option>
+                  <option value="15">15</option>
+                  <option value="20">20</option>
+                  <option value="50">50</option>
+                  <option value="70">70</option>
+                  <option value="100">100</option>
+                </select>
+              </div>
+              <div class="show_product">
+                <table id="table-id">
+                  <tr>
+                    <th>Pickup Date</th>
+                    <th>Pickup Time</th>
+                    <th>Return Date</th>
+                    <th>Return Time</th>
+                    <th>Transfer Type</th>
+                    <th>Payment Status</th>
+                    <th>Total Passengers</th>
+                    <th>Bags</th>
+                    <th>Action</th>
+                  </tr>
+                  <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td>
+                      <a href="#">View Detail</a>
+                    </td>
+                  </tr>
+                </table>
+              </div>
+
+              {/* <!--        Start Pagination --> */}
+              <div class="pagination-container">
+                <nav>
+                  <ul class="pagination">
+                    <li data-page="prev">
+                      <span>
+                        {" "}
+                        <span class="sr-only">(current)</span>
+                      </span>
+                    </li>
+
+                    <li data-page="next" id="prev">
+                      <span>
+                        {" "}
+                        <span class="sr-only">(current)</span>
+                      </span>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+            {/* <!--         End of pagination --> */}
+          </div>
+        </div>
+
+        <_Vehicles />
+
+        <_AddNewTaxi />
+
+        <_ExtrasList />
+
+        <_AddNewExtra />
+      </div>
+      {/* end body */}
+    </AdminLayout>
+  );
+}
+
+function AdminLayout({ children }) {
+  return (
     <section className="dashboard">
       <section className="content">
         <section class="side_bar">
@@ -39,90 +128,7 @@ export default function AdminBookingList() {
           </div>
         </div>
 
-        {/* body */}
-        <div class="body">
-          <div class="admin_container">
-            {/* <!-- dashboard window start--> */}
-            <div class="dashboard_window" id="dashboard_window">
-              <div class="dashboard_form">
-                <strong>Booking History:</strong>
-                <div class="list_selecter">
-                  <select class="form-control" name="state" id="maxRows">
-                    <option value="5000">Show ALL Rows</option>
-                    <option value="5">5</option>
-                    <option value="10">10</option>
-                    <option value="15">15</option>
-                    <option value="20">20</option>
-                    <option value="50">50</option>
-                    <option value="70">70</option>
-                    <option value="100">100</option>
-                  </select>
-                </div>
-                <div class="show_product">
-                  <table id="table-id">
-                    <tr>
-                      <th>Pickup Date</th>
-                      <th>Pickup Time</th>
-                      <th>Return Date</th>
-                      <th>Return Time</th>
-                      <th>Transfer Type</th>
-                      <th>Payment Status</th>
-                      <th>Total Passengers</th>
-                      <th>Bags</th>
-                      <th>Action</th>
-                    </tr>
-                    <tr>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td>
-                        <a href="#">View Detail</a>
-                      </td>
-                    </tr>
-                  </table>
-                </div>
-
-                {/* <!--        Start Pagination --> */}
-                <div class="pagination-container">
-                  <nav>
-                    <ul class="pagination">
-                      <li data-page="prev">
-                        <span>
-                          {" "}
-                          <span class="sr-only">(current)</span>
-                        </span>
-                      </li>
-
-                      <li data-page="next" id="prev">
-                        <span>
-                          {" "}
-                          <span class="sr-only">(current)</span>
-                        </span>
-                      </li>
-                    </ul>
-                  </nav>
-                </div>
-              </div>
-              {/* <!--         End of pagination --> */}
-            </div>
-          </div>
-          {/* <!-- dashboard window end -->  */}
-
-          <_Vehicles />
-          {/* <!-- vender message window end --> */}
-
-          <_AddNewTaxi />
-
-          <_ExtrasList />
-
-          <_AddNewExtra />
-        </div>
-        {/* end body */}
+        {children}
       </section>
     </section>
   );
